4eb92d7cdc6d139ae87834372e5518d8df2a2561
wokka112 <adam_s_poole@hotmail.com>
2023-12-21 20:12:24 +0000

    Add subperiod logic and hardcoded records
      * Hardcoded records added to home view model for prototyping purposes.

--- app/src/main/java/com/floatingpanda/tasktracker/data/task/RepeatableTaskRecord.kt
+++ app/src/main/java/com/floatingpanda/tasktracker/data/task/RepeatableTaskRecord.kt
@@ -8,20 +8,26 @@ import java.time.temporal.ChronoField
 class RepeatableTaskRecord(
     private val template: RepeatableTaskTemplate,
     private val startDate: Long,
+    // TODO endDate seems unnecessary, we can calculate that
     private val endDate: Long,
-    private val completionsPerDate: Map<String, Int>
 ) {
+    private val completionsPerDate: Map<String, Int> = HashMap()
     val isComplete: Boolean
         get() = completionsPerDate.size >= template.timesPerPeriod
 
     fun isCompleteForSubPeriod(): Boolean {
+        if (template.subPeriod == null)
+            return false;
+
         var earlierDayInSubPeriod = datesFromStartOfSubPeriod()
         val today = LocalDate.now()
 
         var completionsInSubPeriod = 0;
         do {
             completionsInSubPeriod += completionsPerDate[earlierDayInSubPeriod.toString()] ?: 0
-            if (completionsInSubPeriod >= template.timesPerSubPeriod)
+            val timesPerSubPeriod = template.timesPerSubPeriod ?: Int.MAX_VALUE
+
+            if (completionsInSubPeriod >= timesPerSubPeriod)
                 return true
 
             earlierDayInSubPeriod = earlierDayInSubPeriod.plusDays(1)
--- app/src/main/java/com/floatingpanda/tasktracker/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/floatingpanda/tasktracker/ui/home/HomeViewModel.kt
@@ -1,8 +1,10 @@
 package com.floatingpanda.tasktracker.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.floatingpanda.tasktracker.data.Day
 import com.floatingpanda.tasktracker.data.Period
 import com.floatingpanda.tasktracker.data.task.RepeatableTaskRecord
+import com.floatingpanda.tasktracker.data.task.RepeatableTaskTemplate
 import com.floatingpanda.tasktracker.data.task.TaskDetails
 import java.util.Calendar
 
@@ -20,29 +22,110 @@ class HomeViewModel : ViewModel() {
         val nextYear = Calendar.getInstance()
         nextYear.add(Calendar.YEAR, 1);
 
-        // I want: a daily record,
-        val wakeUpTemplate = TaskDetails("Wake up", null, "none", Period.DAILY, 1, false);
-        val wakeUpRecord = RepeatableTaskRecord(wakeUpTemplate, today.timeInMillis, tomorrow.timeInMillis, 0);
+        val days = listOf<Day>(
+            Day.MONDAY,
+            Day.TUESDAY,
+            Day.WEDNESDAY,
+            Day.THURSDAY,
+            Day.FRIDAY,
+            Day.SATURDAY,
+            Day.SUNDAY
+        )
+
+        val weekdays = listOf<Day>(
+            Day.MONDAY,
+            Day.TUESDAY,
+            Day.WEDNESDAY,
+            Day.THURSDAY,
+            Day.FRIDAY
+        )
+
+        // a daily record,
+        val wakeUpDetails = TaskDetails("Wake up", null, "none")
+        val wakeUpTemplate =
+            RepeatableTaskTemplate(wakeUpDetails, Period.DAILY, 1, null, null, days)
+        val wakeUpRecord =
+            RepeatableTaskRecord(wakeUpTemplate, today.timeInMillis, tomorrow.timeInMillis)
+
         // a daily record with 3 times in period,
-        val stretchTemplate = TaskDetails("Stretch", null, "Health", Period.DAILY, 3, false);
-        val stretchRecord = RepeatableTaskRecord(stretchTemplate, today.timeInMillis, tomorrow.timeInMillis, 0);
-        // a weekly record,
-        val ocdGroupTemplate = TaskDetails("Attend OCD group", null, "Health", Period.WEEKLY, 1, false)
-        val ocdGroupRecord = RepeatableTaskRecord(ocdGroupTemplate, today.timeInMillis, nextWeek.timeInMillis, 0);
-        // a weekly record with 3 times in period,
-        val runTemplate = TaskDetails("Run", null, "Health", Period.WEEKLY, 3, true);
-        val runRecord = RepeatableTaskRecord(runTemplate, today.timeInMillis, nextWeek.timeInMillis, 0);
-        // a weekly record with 3 times in period and allowing more than once per day,
-        val readTemplate = TaskDetails("Read", null, "Leisure", Period.WEEKLY, 3, false);
-        val readRecord = RepeatableTaskRecord(readTemplate, today.timeInMillis, nextWeek.timeInMillis, 0);
+        val stretchDetails = TaskDetails("Stretch", null, "Health")
+        val stretchTemplate =
+            RepeatableTaskTemplate(stretchDetails, Period.DAILY, 3, null, null, days)
+        val stretchRecord =
+            RepeatableTaskRecord(stretchTemplate, today.timeInMillis, tomorrow.timeInMillis)
+
+        // a weekly record with once per period,
+        val ocdGroupDetails =
+            TaskDetails("Attend OCD group", null, "Health")
+        val ocdGroupTemplate =
+            RepeatableTaskTemplate(ocdGroupDetails, Period.WEEKLY, 1, null, null, days)
+        val ocdGroupRecord =
+            RepeatableTaskRecord(ocdGroupTemplate, today.timeInMillis, nextWeek.timeInMillis)
+
+        // a weekly record with 3 times in period and only once daily, but no weekends
+        val runDetails = TaskDetails("Run", null, "Health")
+        val runTemplate =
+            RepeatableTaskTemplate(runDetails, Period.WEEKLY, 3, Period.DAILY, 1, weekdays)
+        val runRecord =
+            RepeatableTaskRecord(runTemplate, today.timeInMillis, nextWeek.timeInMillis)
+
+        // a weekly record with 3 times in period and allowing twice daily,
+        val readDetails = TaskDetails("Read", null, "Leisure")
+        val readTemplate =
+            RepeatableTaskTemplate(readDetails, Period.WEEKLY, 3, Period.DAILY, 2, days)
+        val readRecord =
+            RepeatableTaskRecord(readTemplate, today.timeInMillis, nextWeek.timeInMillis)
+
+        // a weekly record with 3 times in period and allowing unlimited times daily
+        val walkDetails = TaskDetails("Walk", null, "Leisure")
+        val walkTemplate = RepeatableTaskTemplate(walkDetails, Period.WEEKLY, 3, null, null, days);
+        val walkRecord =
+            RepeatableTaskRecord(walkTemplate, today.timeInMillis, nextWeek.timeInMillis)
+
         // a monthly record,
-        val exploreWildernessTemplate = TaskDetails("Explore the wilderness", null, "Leisure", Period.MONTHLY, 2, true);
-        val exploreWildernessRecord = RepeatableTaskRecord(exploreWildernessTemplate, today.timeInMillis, nextMonth.timeInMillis, 0);
-        // A quarterly record,
-        val breakFromWorkTemplate = TaskDetails("Take a break from work", null, "Leisure", Period.QUARTERLY, 1, true);
+        val exploreWildernessDetails =
+            TaskDetails("Explore the wilderness", null, "Leisure")
+        val exploreWildernessTemplate =
+            RepeatableTaskTemplate(exploreWildernessDetails, Period.MONTHLY, 2, null, null, days)
+        val exploreWildernessRecord = RepeatableTaskRecord(
+            exploreWildernessTemplate,
+            today.timeInMillis,
+            nextMonth.timeInMillis
+        )
+
+        // a monthly record allowing 2 times per week
+        val rosieDetails = TaskDetails("Do stuff with Rosie", null, "Leisure")
+        val rosieTemplate =
+            RepeatableTaskTemplate(rosieDetails, Period.MONTHLY, 4, Period.WEEKLY, 2, days)
+        val rosieRecord =
+            RepeatableTaskRecord(rosieTemplate, today.timeInMillis, nextMonth.timeInMillis)
+
         // a yearly record
-        val holidayTemplate = TaskDetails("Go on holiday", null, "Leisure", Period.YEARLY, 3, true);
+        val holidayDetails = TaskDetails("Go on holiday", null, "Leisure");
+        val holidayTemplate =
+            RepeatableTaskTemplate(holidayDetails, Period.YEARLY, 4, null, null, days)
+        val holidayRecord =
+            RepeatableTaskRecord(holidayTemplate, today.timeInMillis, nextYear.timeInMillis)
 
+        // A yearly record allowing 2 times per month,
+        val breakFromWorkDetails =
+            TaskDetails("Take a break from work", null, "Leisure")
+        val breakFromWorkTemplate =
+            RepeatableTaskTemplate(breakFromWorkDetails, Period.YEARLY, 6, Period.MONTHLY, 2, days)
+        val breakFromWorkRecord =
+            RepeatableTaskRecord(breakFromWorkTemplate, today.timeInMillis, nextYear.timeInMillis)
+
+        records = listOf(
+            wakeUpRecord,
+            stretchRecord,
+            ocdGroupRecord,
+            runRecord,
+            readRecord,
+            walkRecord,
+            exploreWildernessRecord,
+            rosieRecord,
+            holidayRecord,
+            breakFromWorkRecord
+        )
     }
-
 }
