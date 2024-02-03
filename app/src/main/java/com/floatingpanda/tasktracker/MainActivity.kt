f057e5456770aff93a9e0d0c0a78802a5cf6a782
wokka112 <adam_s_poole@hotmail.com>
2024-02-03 19:20:20 +0000

    Add in task creation and schedule fragments
      * Updated nav graph to include new fragments.

--- app/src/main/java/com/floatingpanda/tasktracker/MainActivity.kt
+++ app/src/main/java/com/floatingpanda/tasktracker/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.floatingpanda.tasktracker.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -42,6 +37,10 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            navController.navigate(R.id.action_home_fragment_to_task_creation_create_fragment)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/floatingpanda/tasktracker/ui/creation/CreateTaskFragment.kt
@@ -0,0 +1,68 @@
+package com.floatingpanda.tasktracker.ui.creation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.floatingpanda.tasktracker.R
+
+class CreateTaskFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val taskCreationViewModel =
+            ViewModelProvider(this).get(TaskCreationViewModel::class.java)
+        val view = inflater.inflate(R.layout.fragment_task_creation_create, container)
+
+        setupEditTexts(view, taskCreationViewModel)
+
+        view.findViewById<Button>(R.id.cancel_button).setOnClickListener {
+            findNavController().navigateUp()
+        }
+        view.findViewById<Button>(R.id.continue_button).setOnClickListener {
+            findNavController().navigate(R.id.action_task_creation_create_fragment_to_task_creation_schedule_fragment)
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun setupEditTexts(rootView: View, taskCreationViewModel: TaskCreationViewModel) {
+        val titleInput: EditText = rootView.findViewById(R.id.title_input)
+        val categoryInput: EditText = rootView.findViewById(R.id.category_input)
+        val infoInput: EditText = rootView.findViewById(R.id.info_input)
+
+        taskCreationViewModel.title.observe(this.viewLifecycleOwner) {
+            titleInput.setText(it, TextView.BufferType.EDITABLE)
+        }
+        titleInput.doAfterTextChanged {
+            if (!taskCreationViewModel.title.value.equals(it?.toString()))
+                taskCreationViewModel.title.postValue(it.toString())
+        }
+
+        taskCreationViewModel.category.observe(this.viewLifecycleOwner) {
+            categoryInput.setText(it, TextView.BufferType.EDITABLE)
+        }
+        categoryInput.doAfterTextChanged {
+            if (!taskCreationViewModel.category.value.equals(it?.toString()))
+                taskCreationViewModel.category.postValue(it.toString())
+        }
+
+        taskCreationViewModel.info.observe(this.viewLifecycleOwner) {
+            infoInput.setText(it, TextView.BufferType.EDITABLE)
+        }
+        categoryInput.doAfterTextChanged {
+            if (!taskCreationViewModel.category.value.equals(it?.toString()))
+                taskCreationViewModel.info.postValue(it.toString())
+        }
+    }
+}
